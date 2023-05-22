a5c9b575f77d4214a93545ea0a2ea58b8854fbd3
* <charlivautistahernandez@gmail.com>
2023-05-22 00:16:08 -0500

    V1

--- update.sh
+++ update.sh
@@ -6,15 +6,15 @@ sleep 0.5
 data="$HOME/WhatsPhish"
 
 echo -e '\e[1;32m[\e[1;31m+\e[1;32m] \e[0;35mActualizando WhatsPhish...'
-sleep 0.4 
-rm -rf ${data}
-git clone https:github.com/Charlydev777/WhatsPhish ${data}
+sleep 0.4
+rm -rf "$data"
+git clone https://github.com/Charlydev777/WhatsPhish "$data"
 
-cd ${data}
+cd "$data"
 chmod 777 *.sh
 
 sleep 0.6
-echo -e '\e[1;32m[\e[1;31m+\e[1;32m] \e[0;35mWhatsPhish Actualizado a su version mas reciente.'
+echo -e '\e[1;32m[\e[1;31m+\e[1;32m] \e[0;35mWhatsPhish Actualizado a su versión más reciente.'
 
 #==================================================
 #           Created by: @CharlyDev777
